b5a4e29c9cb7f8e852ad6a422fb50144cc7a5d5e
Iryna Hudymenko <iryn4ik09@gmail.com>
2021-12-23 00:47:54 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,37 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+// const simpleLightBox = require('simplelightbox');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
+
+const palleteContainer = document.querySelector('.gallery');
+const cardsMarkup = createColorCardMarkup();
+
+function createColorCardMarkup() {
+    return galleryItems.map(item =>
+        `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      alt='${item.description}'
+    />
+  </a>`).join('');
+}
+palleteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+    let image = '';
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+palleteContainer.addEventListener('click', onImageToCreateModal);
+    
+    gallery.on('show.simplelightbox', function onImageToCreateModal () {
+        image = 
+        `<div class="gallery">
+    <a href="${item.original}"><img src="
+    ${item.preview}" alt="${item.description}"/></a>
+   
+</div>,`
+    });
+    console.log(image);
