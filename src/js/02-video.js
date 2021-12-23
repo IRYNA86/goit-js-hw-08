f7d481fa527a5131a34828364d7a3b42edb8ec09
Iryna Hudymenko <iryn4ik09@gmail.com>
2021-12-24 01:36:49 +0200

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,17 @@
 import Player from "@vimeo/player";
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframeRef = document.querySelector('iframe');
 const player = new Vimeo.Player(iframeRef);
 iframeRef.addEventListener('play', onVideoStart);
-player.on('play',
-    function throttle () {
+player.on('play', throttle(function () {
    
         player.getCurrentTime().then(function (time) {
             console.log('title', time);
             localStorage.setItem('current-time', JSON.stringify(time))
         });
 
-    } , 1000);
+    }) ,1000);
 
 const toStart = JSON.parse(localStorage.getItem('current-time'));
 console.log(toStart);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+const storageKey = 'feedback-form-state';
+inItForm();
+
+form.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+    event.preventDefault();
+    const userData = {};
+    const formData = new FormData(form);
+    const formElemenls = event.currentTarget.elements;
+    const formEmail = formElemenls.email.value;
+    const formMessage = formElemenls.message.value;
+    if (!formMessage || !formEmail) {
+        return alert('Fill in the missing information')
+    }
+    formData.forEach((value, name) => 
+        userData[name] = value);
+    console.log(userData);
+    event.currentTarget.reset();
+    localStorage.removeItem(storageKey);    
+};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+    let saveData = localStorage.getItem(storageKey);
+    saveData = saveData ? JSON.parse(saveData) : {};
+    saveData[event.target.name] = event.target.value;
+    localStorage.setItem(storageKey, JSON.stringify(saveData));
+};
+function inItForm() {
+    let saveData = localStorage.removeItem(storageKey);
+    if (saveData) {
+        saveData = JSON.parse(saveData)
+        Object.entries(saveData).forEach(([name, value]) => {
+            form.elements[name].value = value;
+    })
+    };
+        
+    }
