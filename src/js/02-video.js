6c84774bcb132a6a30eee80cc5bc8163835a9d67
Iryna Hudymenko <iryn4ik09@gmail.com>
2021-12-23 23:37:01 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from "@vimeo/player";
+const throttle = require('lodash.throttle');
 
+const iframeRef = document.querySelector('iframe');
+const player = new Vimeo.Player(iframeRef);
+iframeRef.addEventListener('play', onVideoStart);
+player.on('play',
+    function throttle () {
+   
+        player.getCurrentTime().then(function (time) {
+            console.log('title', time);
+            localStorage.setItem('current-time', JSON.stringify(time))
+        });
+
+    } , 1000);
+
+const toStart = JSON.parse(localStorage.getItem('current-time'));
+console.log(toStart);
+player.setCurrentTime(toStart);
+
+function onVideoStart(toStart) {
+    console.log(toStart);
+        }
